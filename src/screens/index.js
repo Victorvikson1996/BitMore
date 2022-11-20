a016aa2102703749f7aabbcb87604930e6bf12f7
Victor Ezea <victorvikson@gmail.com>
2022-11-21 00:59:24 +0100

    Scrren setUp

---
+++ src/screens/index.js
@@ -0,0 +1,15 @@
+import ConfirmScreen from './ConfirmScreen';
+import HomeScreen from './HomeScreen';
+import LoginScreen from './LoginScreen';
+import PendingScreen from './PendingScreen';
+import SendScreen from './SendScreen';
+import SplashScreen from './SplashScreen';
+
+export {
+  ConfirmScreen,
+  HomeScreen,
+  LoginScreen,
+  PendingScreen,
+  SendScreen,
+  SplashScreen,
+};
